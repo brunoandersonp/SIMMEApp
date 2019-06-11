00c43e4b87536fa15f7c75007b3905d85d0c5fe7
brunoandersonp <brunoandersonp@gmail.com>
2019-06-11 10:53:06 -0300

    Timer Asynchronous Task

--- app/src/main/java/com/example/finalsimme/Fragment/GraficosFragment.java
+++ app/src/main/java/com/example/finalsimme/Fragment/GraficosFragment.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ import com.example.finalsimme.R;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -37,15 +39,17 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GraficosFragment extends Fragment{
 
-    //TextView textGrafico;
+    TextView textGrafico;
 
-    private LineChart grafico;
+//    private LineChart grafico;
 
 
     private String idBanco, idEquipamento;
@@ -82,88 +86,119 @@ public class GraficosFragment extends Fragment{
             idEquipamento = getArguments().getString(equipamento);
         }
 
-        //textGrafico = view.findViewById(R.id.textGrafico);
+        textGrafico = view.findViewById(R.id.textGrafico);
 
-        grafico = view.findViewById(R.id.lineChart);
+//        grafico = view.findViewById(R.id.lineChart);
 
         Log.d("list graficos", PontosListAdapter.pontosSelecionados.toString());
 
 //        grafico.setOnChartGestureListener(GraficosFragment.this);
 //        grafico.setOnChartValueSelectedListener(GraficosFragment.this);
 
-        grafico.setDragEnabled(true);
-        grafico.setScaleEnabled(true);
-
-        ArrayList<Entry> yValues = new ArrayList<>();
-
-        yValues.add(new Entry(0, 60f));
-        yValues.add(new Entry(1, 50f));
-        yValues.add(new Entry(2, 40f));
-        yValues.add(new Entry(3, 50f));
-        yValues.add(new Entry(4, 35f));
-        yValues.add(new Entry(5, 40f));
-        yValues.add(new Entry(0, 60f));
-
-        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
-
-        set1.setFillAlpha(110);
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
-
-        LineData data = new LineData(dataSets);
-
-        grafico.setData(data);
+//        grafico.setDragEnabled(true);
+//        grafico.setScaleEnabled(true);
+//
+//        LimitLine upper_limit = ;
+//
+//        ArrayList<Entry> yValues = new ArrayList<>();
+//
+//        yValues.add(new Entry(0, 60f));
+//        yValues.add(new Entry(1, 50f));
+//        yValues.add(new Entry(2, 40f));
+//        yValues.add(new Entry(3, 50f));
+//        yValues.add(new Entry(4, 35f));
+//        yValues.add(new Entry(5, 40f));
+//        yValues.add(new Entry(0, 60f));
+//
+//        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+//
+//        set1.setFillAlpha(110);
+//
+//        set1.setColor(Color.RED);
+//        set1.setLineWidth(3f);
+//        set1.setValueTextSize(10f);
+//
+//        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+//        dataSets.add(set1);
+//
+//        LineData data = new LineData(dataSets);
+//
+//        grafico.setData(data);
 
 //        GerarGraficos gerarGraficos = new GerarGraficos();
 //        gerarGraficos.execute();
 
+        callAsynchronousTask();
+
         return view;
     }
 
-//    public class GerarGraficos extends AsyncTask<Void, Void, Void>{
-//
-//        List<String> pontosEscolhidos = new ArrayList<>();
-//
-//        @Override
-//        protected Void doInBackground(Void... voids) {
-//
-//            GetData ponto = new GetData();
-//            String pontos = ponto.getDataSOAPPonto("JsonCPonto", idBanco, idEquipamento);
-//
-//            if(pontos != null){
-//                try{
-//                    pontosEscolhidos.clear();
-//                    JSONArray JA = new JSONArray(pontos);
-//                    for(int i = 0; i < JA.length(); i++){
-//                        JSONObject JO = (JSONObject) JA.get(i);
-//                        if(PontosListAdapter.pontosSelecionados.contains(String.valueOf(JO.getInt("cPontoID")))){
-//                               pontosEscolhidos.add(JO.getString("Descr"));
-//                        }
-//                    }
-//
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void aVoid) {
-//            super.onPostExecute(aVoid);
-//
-//            Log.d("pontos escolhidos", pontosEscolhidos.toString());
-//
-//            textGrafico.setText("");
-//
-//            String teste = "";
-//            for(int i = 0; i < pontosEscolhidos.size(); i++){
-//                teste += pontosEscolhidos.get(i) + "\n";
-//                textGrafico.setText(teste);
-//            }
-//        }
-//    }
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            GerarGraficos gerarGraficos = new GerarGraficos();
+                            // PerformBackgroundTask this class is the class that extends AsynchTask
+                            gerarGraficos.execute();
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 0, 1000); //execute in every 50000 ms
+    }
+
+
+    public class GerarGraficos extends AsyncTask<Void, Void, Void>{
+
+        List<String> pontosEscolhidos = new ArrayList<>();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            GetData ponto = new GetData();
+            String pontos = ponto.getDataSOAPPonto("JsonCPonto", idBanco, idEquipamento);
+
+            if(pontos != null){
+                try{
+                    pontosEscolhidos.clear();
+                    JSONArray JA = new JSONArray(pontos);
+                    for(int i = 0; i < JA.length(); i++){
+                        JSONObject JO = (JSONObject) JA.get(i);
+                        if(PontosListAdapter.pontosSelecionados.contains(String.valueOf(JO.getInt("cPontoID")))){
+                               pontosEscolhidos.add(JO.getString("valorglobal"));
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            Log.d("pontos escolhidos", pontosEscolhidos.toString());
+
+            textGrafico.setText("");
+
+            String teste = "";
+            for(int i = 0; i < pontosEscolhidos.size(); i++){
+                teste += pontosEscolhidos.get(i) + "\n";
+                textGrafico.setText(teste);
+            }
+        }
+    }
 
 }
